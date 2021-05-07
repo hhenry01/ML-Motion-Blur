12039e9dfa4d08dc1138a8b52d2ad90bb5f45821
Mieszko <mieszko>
2021-05-07 11:55:51 -0700

    Google compute keys

--- main_site/validators.py
+++
@@ -1,7 +0,0 @@
-def validate_file_extension(value):
-    import os
-    from django.core.excepts import ValidationError
-    ext = os.path.splitext(value.name)[1] # returns path + filename
-    valid_extensions = ['.mp4', '.mov', '.wmv', '.avi']
-    if not ext.lower() in valid_extensions:
-        raise ValidationError('Unsupported file extension.')
