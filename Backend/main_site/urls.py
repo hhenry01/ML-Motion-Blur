88245ab4c9759da41efeb349da74b395949cd233
Ivan Guo <ivanguo248@gmail.com>
2021-05-08 16:19:35 -0700

    fixed backend folders, all files are up to date

--- Backend/main_site/urls.py
+++ Backend/main_site/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='main_site-home'),
+    path('about/', views.about, name='main_site-about'),
 ]
--- Backend/main_site/views.py
+++ Backend/main_site/views.py
@@ -1,21 +1,31 @@
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
+# from . import tracking
+import os
+import pathlib
+from pathlib import Path
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
     context = {}
     if request.method == 'POST':
-        # uploaded_file = request.FILES['video']
         uploaded_file = request.FILES['document']
         fs = FileSystemStorage()
-        name = fs.save(uploaded_file.name, uploaded_file)
+        name = fs.save('video', uploaded_file)
         url = fs.url(name)
         context['url'] = fs.url(name)
+        return processVid(request)
+    
     return render(request, 'main_site/home.html', context)
 
-# NEW
-# def video_list(request):
-#     return render(request, 'video_list.html')
+def about(request):
+    return render(request, 'main_site/about.html')
 
-# def upload_video(request):
-#     return render(request, 'upload_video.html')
+def processVid(request):
+    parent_dir = pathlib.Path('views.py').parent.absolute()
+    path = f'{parent_dir}/media/video'
+    # model = tracking.load_model('Model.pt', len(tracking.classes) + 1)
+    # detections, labels = tracking.track(model, path)
+
+    return HttpResponse("<h1>Test page to process the video</h1>")
