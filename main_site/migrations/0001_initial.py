b3c1824ea40c70d8ab1a500b9413558f86f0bb64
Ivan Guo <ivanguo248@gmail.com>
2021-05-08 15:11:01 -0700

    updated files so that there is a better interface on the website, a video embed in the home page, and the ability to find path where video is stored so we can begin sending the model the video for processing

--- main_site/migrations/0001_initial.py
+++ main_site/migrations/0001_initial.py
@@ -15,8 +15,8 @@ class Migration(migrations.Migration):
             name='Video',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=100)),
-                ('url', models.FileField(upload_to='videos/urls/')),
+                ('name', models.CharField(max_length=500)),
+                ('videofile', models.FileField(upload_to='videos/')),
             ],
         ),
     ]
--- main_site/views.py
+++ main_site/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
-from torch import nn, optim
-from torchvision import datasets, models, transforms
+# from . import tracking
+import os
+import pathlib
+from pathlib import Path
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
@@ -9,15 +12,20 @@ def home(request):
     if request.method == 'POST':
         uploaded_file = request.FILES['document']
         fs = FileSystemStorage()
-        name = fs.save(uploaded_file.name, uploaded_file)
+        name = fs.save('video', uploaded_file)
         url = fs.url(name)
         context['url'] = fs.url(name)
-
+        return processVid(request)
+    
     return render(request, 'main_site/home.html', context)
 
 def about(request):
     return render(request, 'main_site/about.html')
 
 def processVid(request):
-    model=torch.load('Model.pt', map_location=torch.device('cpu'))
-    model.eval()
+    parent_dir = pathlib.Path('views.py').parent.absolute()
+    path = f'{parent_dir}/media/video'
+    # model = tracking.load_model('Model.pt', len(tracking.classes) + 1)
+    # detections, labels = tracking.track(model, path)
+
+    return HttpResponse("<h1>Test page to process the video</h1>")
